c05d03b0c23c4bef68461cd8628b0a64363357a3
AHSEN <ahsenikhan@gmail.com>
2024-10-12 18:38:19 +0500

    solved the theme issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme/ThemeProvider"
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700', '800', '900'],
@@ -22,12 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${poppins.className} bg-background`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-        >
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html >
   );
